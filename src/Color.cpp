7e8a06db66aa11e42f7ee3780fdbcaff298de1f8
All23tor <acc.dummyagta1@gmail.com>
2025-04-02 20:14:38 -0500

    Simplified Color class construction by hue

--- src/Color.cpp
+++ src/Color.cpp
@@ -2,45 +2,30 @@
 #include <cmath>
 
 Color::Color(unsigned char r, unsigned char g, unsigned char b) :
-  R(r), G(g), B(b) {}
+    R(r),
+    G(g),
+    B(b) {}
 
 Color::Color(double hue) {
   unsigned char x = 255 * (1 - std::abs(std::fmod(hue / 60, 2) - 1));
-  switch (int(hue) / 60) {
+  int sector = (int(hue) / 60);
+  switch (sector / 2) {
   case 0:
-    R = 255;
-    G = x;
+    R = sector % 2 ? x : 255;
+    G = sector % 2 ? 255 : x;
     B = 0;
     break;
 
   case 1:
-    R = x;
-    G = 255;
-    B = 0;
+    R = 0;
+    G = sector % 2 ? x : 255;
+    B = sector % 2 ? 255 : x;
     break;
 
   case 2:
-    R = 0;
-    G = 255;
-    B = x;
-    break;
-
-  case 3:
-    R = 0;
-    G = x;
-    B = 255;
-    break;
-
-  case 4:
-    R = x;
+    R = sector % 2 ? 255 : x;
     G = 0;
-    B = 255;
-    break;
-
-  case 5:
-    R = 255;
-    G = 0;
-    B = x;
+    B = sector % 2 ? x : 255;
     break;
   }
 }
